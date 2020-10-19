dc3350e3842d4d5b5797808be45474fa4cb36913
Little Seahorse <vytas@littleseahorsemedia.co.uk>
2020-10-19 16:35:22 +0100

    Completed set-up of back-end server using express.

---
+++ back-end/src/App.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'Hello World!',
+  });
+});
+
+app.listen(process.env.PORT || 8081);
